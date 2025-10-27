81bf7dbcef674cca92b8d95122c6fee485eabecf
Danya <danilbaydimirov@gmail.com>
2025-10-27 19:39:26 +0500

    ToDo 1.2

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-const btn_theme = document.querySelector('.btn_theme');
-
-btn_theme.addEventListener('click', function (event) {
+let dark_theme = false;
+document.querySelector('.btn_theme').addEventListener('click', function (event) {
     event.preventDefault();
-    document.body.classList.toggle('body-dark')
+    document.querySelector('.label_text').classList.toggle('label-dark');
+    document.body.classList.toggle('body-dark');
+    dark_theme = document.body.classList.contains('body-dark') == true ? true : false;
 })
-
 function checkbox_change(index) {
     let label = document.querySelector(`#label_${index}`)
     if (document.querySelector(`#checkbox_${index}`).checked) {
         label.style.textDecoration = 'line-through';
-        label.style.color = '#252525';
         label.style.opacity = '50%';
     } else {
         label.style.textDecoration = 'none';
-        label.style.color = 'black';
         label.style.opacity = '100%';
+    }
+    if (dark_theme) {
+        document.querySelector('.label_text').classList.add('label-dark');
+    } else {
+        document.querySelector('.label_text').classList.remove('label-dark');
     }
 }
