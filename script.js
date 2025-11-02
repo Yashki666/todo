09e763e969f309ec188489b054b21e3d5b13e59b
Danya <danilbaydimirov@gmail.com>
2025-11-02 22:20:00 +0500

    ToDo 2.0

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ let background_dark = document.querySelector('.background-dark-theme-menu');
 let menu = document.querySelector('.div-menu-add-note');
 let menu_edit = document.querySelector('.div-menu-edit-note');
 let option = document.querySelector('.option-edit-note-list1');
-option.addEventListener('click', () => {
-    console.log(1);
-})
 let select = document.querySelector('.select-all');
-let temp_i = document.querySelector('.div-note-and-hr').children.length + 1;
+let temp_i = document.querySelector('.div-note-and-hr').children.length - 1;
 let temp_index;
+let remote_note;
+let searchInput = document.querySelector('.input-search-note');
+let filterBtn = document.getElementById('filterBtn');
+let dropdown = document.getElementById('filterDropdown');
+let btnDropdown = document.querySelector('.btn-dropdown');
+let textDropdown = document.querySelector('.textDropdown');
+let noteList = document.querySelector('.div-note-list');
 
+// Добавления нового пункта.
 document.querySelector('.btn-add-note').addEventListener('click', () => {
     background_dark.style.display = 'flex';
     background_dark.style.opacity = '0';
@@ -20,10 +25,11 @@ document.querySelector('.btn-add-note').addEventListener('click', () => {
     }, 10);
 })
 
+// Смена темы на темную и наоборот.
 document.querySelector('.btn-change-theme').addEventListener('click', () => {
     document.body.classList.toggle('body-dark');
     document.querySelector('.btn-change-theme').classList.toggle('btn-change-theme-dark');
-    document.querySelector('.input-search-note').classList.toggle('input-search-note-dark');
+    searchInput.classList.toggle('input-search-note-dark');
     document.querySelector('.btn-search-note').classList.toggle('btn-search-note-dark');
     document.querySelector('.div-menu-add-note').classList.toggle('div-menu-add-note-dark');
     document.querySelector('.div-menu-edit-note').classList.toggle('div-menu-edit-note-dark');
@@ -32,14 +38,18 @@ document.querySelector('.btn-change-theme').addEventListener('click', () => {
     document.querySelector('.detective').classList.toggle('detective-dark');
 })
 
+// Зачеркивание пункта при активированном флажке.
 function checked_checkbox(index) {
     if (document.querySelector(`#checkbox-${index}`).checked) {
         document.querySelector(`#label_note_${index}`).style.textDecoration = 'line-through';
+        document.querySelector(`#checkbox-${index}`).setAttribute('checked', 'checked');
     } else {
         document.querySelector(`#label_note_${index}`).style.textDecoration = 'none';
+        document.querySelector(`#checkbox-${index}`).removeAttribute('checked');
     }
 }
 
+// Функция кнопки отмены при добавлении или изменения пункта.
 function btn_cancel() {
     menu.style.opacity = '0';
     menu_edit.style.opacity = '0';
@@ -51,9 +61,9 @@ function btn_cancel() {
     }, 300);
 }
 
-
+// Кнопка подверждения для создания нового пункта в лист.
 document.querySelector('.btn-apply').addEventListener('click', () => {
-    let length_list = document.querySelector('.div-note-list').children.length;
+    let length_list = noteList.children.length;
     let value_input = document.querySelector('.input-add-note').value;
     let clone = `
     <div class="div-note-and-hr" id="div-note-${temp_i}">
@@ -71,15 +81,15 @@ document.querySelector('.btn-apply').addEventListener('click', () => {
     </div>
     `
     if (length_list > 0 && length_list < 7) {
-        document.querySelector('.div-note-list').insertAdjacentHTML('beforeend', clone);
+        noteList.insertAdjacentHTML('beforeend', clone);
         document.querySelector(`#label_note_${temp_i}`).textContent = value_input;
         temp_i++;
     } else {
         alert('Много заметок!');
     }
-    if (document.querySelector('.div-note-list').children.length != 1) {
+    if (noteList.children.length != 1) {
         document.querySelector('.div-detective-empty').style.display = 'none';
-        document.querySelector('.div-note-list').style.display = 'block';
+        noteList.style.display = 'block';
     }
 
     menu.style.opacity = '0';
@@ -89,15 +99,37 @@ document.querySelector('.btn-apply').addEventListener('click', () => {
         menu.style.display = 'none';
     }, 300);
 })
+
+// Удаления пункта
 function delete_note(index) {
+    remote_note = document.querySelector(`#div-note-${index}`).outerHTML;
+    document.querySelector(`.hr-${index}`).style.display = 'none';
     document.querySelector(`#div-note-${index}`).remove();
-    if (document.querySelector('.div-note-list').children.length == 1) {
-        document.querySelector('.div-detective-empty').style.display = 'flex';
-        document.querySelector('.div-note-list').style.display = 'flex';
-    }
-    temp_i--;
+    let undo = `
+    <div class="div-undo" id="div-indo-${index}" onclick="returnNote()">
+        <div class="loader-div">
+            <div class="loader"></div>
+            <p class="timer">2</p>
+        </div>
+        <div>
+            <p>UNDO <img src="files/undo.svg"></p>
+        </div>
+    </div>
+    `
+    noteList.insertAdjacentHTML('beforeend', undo);
     timer_undo();
+    setTimeout(() => {
+        if (document.querySelector('.div-note-and-hr') == null) {
+            document.querySelector('.div-detective-empty').style.display = 'flex';
+            noteList.style.display = 'flex';
+            console.log(1);
+        }
+        document.querySelector(`#div-indo-${index}`).remove();
+    }, 2000);
+    temp_i--;
 }
+
+// Изменение пункта.
 function edit_note(index) {
     console.log(index);
     background_dark.style.display = 'flex';
@@ -110,6 +142,8 @@ function edit_note(index) {
     }, 10);
     temp_index = index;
 }
+
+// Кнопка подверждение на изменение пункта.
 document.querySelector('.btn-apply-edit').addEventListener('click', () => {
     document.querySelector(`#label_note_${temp_index}`).textContent = document.querySelector('.input-edit-note').value;
     menu_edit.style.opacity = '0';
@@ -121,10 +155,11 @@ document.querySelector('.btn-apply-edit').addEventListener('click', () => {
     document.querySelector('.input-edit-note').value = '';
 })
 
+// Кнопка поиска пункта по названию.
 function btn_search() {
     document.querySelectorAll('.div-note-and-hr').forEach((elem, i) => {
         let temp_text_label = document.querySelector(`#label_note_${i}`).textContent.toUpperCase();
-        let temp_search_input = document.querySelector('.input-search-note').value.toUpperCase();
+        let temp_search_input = searchInput.value.toUpperCase();
         console.log(temp_text_label);
         console.log(temp_search_input);
         if (temp_search_input == '') {
@@ -137,18 +172,41 @@ function btn_search() {
     })
 }
 
-select.addEventListener('change', () => {
-    console.log(select.value);
+// Фильтрация списка на выполненные и невыполненные. 
+filterBtn.onclick = function () {
+    dropdown.classList.toggle("open");
+    btnDropdown.classList.toggle('actived');
+}
+function filterDropdown(valueDropdown) {
+    textDropdown.textContent = valueDropdown.toUpperCase();
+    dropdown.classList.remove("open");
+    btnDropdown.classList.remove('actived');
     document.querySelectorAll('.input-checkbox').forEach((element, i) => {
-        if (select.value == 'complete' && !element.checked) {
-            document.querySelector(`#div-note-${i}`).style.display = 'none';
-        } else if (select.value == 'incomplete' && element.checked) {
-            document.querySelector(`#div-note-${i}`).style.display = 'none';
-        } else if (select.value == 'all') {
+        if (valueDropdown == 'complete') {
+            if (element.checked) {
+                document.querySelector(`#div-note-${i}`).style.display = 'flex';
+            } else {
+                document.querySelector(`#div-note-${i}`).style.display = 'none';
+            }
+            btnDropdown.style.width = "120px";
+            searchInput.style.width = "569px";
+        } else if (valueDropdown == 'incomplete') {
+            if (element.checked) {
+                document.querySelector(`#div-note-${i}`).style.display = 'none';
+            } else {
+                document.querySelector(`#div-note-${i}`).style.display = 'flex';
+            }
+            btnDropdown.style.width = "130px";
+            searchInput.style.width = "559px";
+        } else {
             document.querySelector(`#div-note-${i}`).style.display = 'flex';
+            btnDropdown.style.width = "94px";
+            searchInput.style.width = "595px";
         }
-    });
-})
+    })
+}
+
+// Функция таймера для отмены удаления.
 function timer_undo() {
     let i = 2;
     document.querySelector('.timer').textContent = i;
@@ -163,5 +221,26 @@ function timer_undo() {
             document.querySelector('.div-undo').style.display = 'none';
             clearInterval(timer);
         }
+        console.log(2);
+
     }, 650);
 }
+
+// Кнопка отмены удаления.
+function returnNote() {
+    if (remote_note) {
+        noteList.insertAdjacentHTML('beforeend', remote_note);
+        temp_i++;
+        document.querySelector('.loader').style.animation = 'none';
+        document.querySelector('.div-undo').style.display = 'none';
+    }
+}
+
+// document.querySelector('.div-undo').addEventListener('click', () => {
+//     if (remote_note) {
+//         noteList.insertAdjacentHTML('beforeend', remote_note);
+//         temp_i++;
+//         document.querySelector('.loader').style.animation = 'none';
+//         document.querySelector('.div-undo').style.display = 'none';
+//     }
+// })
