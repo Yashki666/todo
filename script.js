321ef0661d26d573032b2cbeceb9acd8d8e52d6f
Danya <danilbaydimirov@gmail.com>
2025-11-17 10:29:51 +0500

    ToDo 2.4

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ let menu = document.querySelector('.div-menu-add-note');
 let menu_edit = document.querySelector('.div-menu-edit-note');
 let option = document.querySelector('.option-edit-note-list1');
 let select = document.querySelector('.select-all');
-let temp_i = document.querySelector('.div-note-and-hr').children.length - 1;
+let temp_i = document.querySelector('.div-note-list').children.length - 1;
 let temp_index;
 let remote_note;
+let tempLanguage = false;
 let searchInput = document.querySelector('.input-search-note');
 let filterBtn = document.getElementById('filterBtn');
 let dropdown = document.getElementById('filterDropdown');
 let btnDropdown = document.querySelector('.btn-dropdown');
 let textDropdown = document.querySelector('.textDropdown');
 let noteList = document.querySelector('.div-note-list');
+let btnChangeLanguage = document.querySelector('.btn-change-language');
+emptyCheck();
 
 // Добавления нового пункта.
 document.querySelector('.btn-add-note').addEventListener('click', () => {
@@ -36,6 +39,7 @@ document.querySelector('.btn-change-theme').addEventListener('click', () => {
     document.querySelector('.input-add-note').classList.toggle('input-add-note-dark');
     document.querySelector('.input-edit-note').classList.toggle('input-edit-note-dark');
     document.querySelector('.detective').classList.toggle('detective-dark');
+    document.querySelector('.menu').classList.toggle('menu-dark');
 })
 
 // Зачеркивание пункта при активированном флажке.
@@ -83,6 +87,8 @@ document.querySelector('.btn-apply').addEventListener('click', () => {
     if (length_list > 0 && length_list < 7) {
         if (value_input.length > 50) {
             alert("Слишком длинный пункт!");
+        } else if (value_input.length == 0) {
+            alert("Пустой пункт!");
         } else {
             noteList.insertAdjacentHTML('beforeend', clone);
             document.querySelector(`#label_note_${temp_i}`).textContent = value_input;
@@ -100,7 +106,6 @@ document.querySelector('.btn-apply').addEventListener('click', () => {
     }
     if (noteList.children.length != 1) {
         document.querySelector('.div-detective-empty').style.display = 'none';
-        noteList.style.display = 'block';
     }
 })
 
@@ -116,21 +121,37 @@ function delete_note(index) {
             <p class="timer">2</p>
         </div>
         <div>
-            <p>UNDO <img src="files/undo.svg"></p>
+            <p class="textUndo">UNDO <img src="files/undo.svg"></p>
         </div>
     </div>
     `
     noteList.insertAdjacentHTML('beforeend', undo);
-    timer_undo();
-    setTimeout(() => {
-        if (document.querySelector('.div-note-and-hr') == null) {
-            document.querySelector('.div-detective-empty').style.display = 'flex';
-            noteList.style.display = 'flex';
-            console.log(1);
+    document.querySelector('.textUndo').innerText = tempLanguage == false ? 'UNDO' : 'ОТМЕНА';
+    let i = 2;
+    document.querySelector('.timer').textContent = i;
+    document.querySelector('.div-undo').style.display = 'flex';
+    document.querySelector('.loader').style.animation = 'l1 2s infinite linear';
+    let timer = setInterval(() => {
+        i--;
+        document.querySelector('.timer').textContent = i;
+        if (i <= -1) {
+            document.querySelector('.loader').style.animation = 'none';
+            document.querySelector('.div-undo').style.display = 'none';
+            clearInterval(timer);
+            document.querySelector(`#div-indo-${index}`).remove();
         }
-        document.querySelector(`#div-indo-${index}`).remove();
-    }, 2000);
+    }, 650);
     temp_i--;
+}
+
+// Кнопка отмены удаления.
+function returnNote() {
+    if (remote_note) {
+        noteList.insertAdjacentHTML('beforeend', remote_note);
+        temp_i++;
+        document.querySelector('.loader').style.animation = 'none';
+        document.querySelector('.div-undo').style.display = 'none';
+    }
 }
 
 // Изменение пункта.
@@ -151,6 +172,8 @@ function edit_note(index) {
 document.querySelector('.btn-apply-edit').addEventListener('click', () => {
     if (document.querySelector('.input-edit-note').value.length > 50) {
         alert("Слишком длинный пункт!");
+    } else if (document.querySelector('.input-edit-note').value.length == 0) {
+        alert("Пустой пункт!");
     } else {
         document.querySelector(`#label_note_${temp_index}`).textContent = document.querySelector('.input-edit-note').value;
         menu_edit.style.opacity = '0';
@@ -159,7 +182,7 @@ document.querySelector('.btn-apply-edit').addEventListener('click', () => {
             background_dark.style.display = 'none';
             menu_edit.style.display = 'none';
         }, 300);
-        document.querySelector('.input-edit-note').value = '';    
+        document.querySelector('.input-edit-note').value = '';
     }
 });
 
@@ -185,70 +208,104 @@ filterBtn.onclick = function () {
     dropdown.classList.toggle("open");
     btnDropdown.classList.toggle('actived');
 }
-function filterDropdown(valueDropdown) {
-    textDropdown.textContent = valueDropdown.toUpperCase();
+function filterDropdown(valueDropdown, valueDropdown_ru) {
+    textDropdown.textContent = tempLanguage == false ? valueDropdown : valueDropdown_ru;
     dropdown.classList.remove("open");
     btnDropdown.classList.remove('actived');
-    document.querySelectorAll('.input-checkbox').forEach((element, i) => {
-        if (valueDropdown == 'complete') {
+    filterNotes();
+    changeWidthMenu();
+}
+
+function filterNotes() {
+    document.querySelectorAll('.div-note-and-hr').forEach((element, i) => {
+        if (textDropdown.textContent == 'COMPLETE' || textDropdown.textContent == 'ВЫПОЛНЕННЫЕ') {
             if (element.checked) {
                 document.querySelector(`#div-note-${i}`).style.display = 'flex';
             } else {
                 document.querySelector(`#div-note-${i}`).style.display = 'none';
             }
-            btnDropdown.style.width = "120px";
-            searchInput.style.width = "569px";
-        } else if (valueDropdown == 'incomplete') {
+        } else if (textDropdown.textContent == 'INCOMPLETE' || textDropdown.textContent == 'НЕВЫПОЛНЕННЫЕ') {
             if (element.checked) {
                 document.querySelector(`#div-note-${i}`).style.display = 'none';
             } else {
                 document.querySelector(`#div-note-${i}`).style.display = 'flex';
             }
-            btnDropdown.style.width = "130px";
-            searchInput.style.width = "559px";
         } else {
             document.querySelector(`#div-note-${i}`).style.display = 'flex';
-            btnDropdown.style.width = "94px";
-            searchInput.style.width = "595px";
         }
-    })
+    });
 }
 
-// Функция таймера для отмены удаления.
-function timer_undo() {
-    let i = 2;
-    document.querySelector('.timer').textContent = i;
-    document.querySelector('.div-undo').style.display = 'flex';
-    document.querySelector('.loader').style.animation = 'l1 2s infinite linear';
-    const timer = setInterval(() => {
-        i--;
-        document.querySelector('.timer').textContent = i;
-
-        if (i <= -1) {
-            document.querySelector('.loader').style.animation = 'none';
-            document.querySelector('.div-undo').style.display = 'none';
-            clearInterval(timer);
-        }
-        console.log(2);
-
-    }, 650);
+function emptyCheck() {
+    if (document.querySelector('.div-note-list').children.length == 1) {
+        document.querySelector('.div-detective-empty').style.display = 'flex';
+    }
+    console.log(document.querySelector('.div-note-list').children.length);
 }
 
-// Кнопка отмены удаления.
-function returnNote() {
-    if (remote_note) {
-        noteList.insertAdjacentHTML('beforeend', remote_note);
-        temp_i++;
-        document.querySelector('.loader').style.animation = 'none';
-        document.querySelector('.div-undo').style.display = 'none';
+btnChangeLanguage.addEventListener('click', () => {
+    tempLanguage = btnChangeLanguage.innerText == 'EN' ? true : false;
+    document.querySelector('.div-menu-add-note h2').innerText = btnChangeLanguage.innerText == 'EN' ? 'НОВАЯ ЗАМЕТКА' : "NEW NOTE";
+    document.querySelector('.btn-cancel>p').innerText = btnChangeLanguage.innerText == 'EN' ? 'ОТМЕНА' : "CANCEL";
+    document.querySelector('.btn-apply>p').innerText = btnChangeLanguage.innerText == 'EN' ? 'ПРИНЯТЬ' : "APPLY";
+    document.querySelector('.div-menu-edit-note h2').innerText = btnChangeLanguage.innerText == 'РЕДАКТИРОВАТЬ ЗАМЕТКУ' ? 'RU' : "EDIT NOTE";
+    document.querySelector('.btn-apply-edit>p').innerText = btnChangeLanguage.innerText == 'EN' ? 'ПРИНЯТЬ' : "APPLY";
+    document.querySelector('.div-h2-and-form>h2').innerText = btnChangeLanguage.innerText == 'EN' ? 'ТОДО СПИСОК' : "TODO LIST";
+    let inputSearch = btnChangeLanguage.innerText == 'EN' ? 'Поиск заметки...' : 'Search note...';
+    let inputAddEdit = btnChangeLanguage.innerText == 'EN' ? 'Введите заметку...' : 'Search note...';
+    document.querySelector('.input-search-note').setAttribute('placeholder', inputSearch);
+    document.querySelector('.input-add-note').setAttribute('placeholder', inputAddEdit);
+    document.querySelector('.input-edit-note').setAttribute('placeholder', inputAddEdit);
+    document.querySelector('.menu-option:first-child').innerText = btnChangeLanguage.innerText == 'EN' ? 'Все' : "All";
+    document.querySelector('.menu-option:nth-child(2)').innerText = btnChangeLanguage.innerText == 'EN' ? 'Выполненные' : "Complete";
+    document.querySelector('.menu-option:last-child').innerText = btnChangeLanguage.innerText == 'EN' ? 'Невыполненные' : "Incomplete";
+    document.querySelector('.empty').innerText = btnChangeLanguage.innerText == 'EN' ? 'Пусто...' : "Empty...";
+    filterNotes();
+    changeLanguageMenuOption();
+    changeWidthMenu()
+    btnChangeLanguage.innerText = btnChangeLanguage.innerText == 'EN' ? 'RU' : 'EN';
+})
+
+function changeLanguageMenuOption() {
+    if (textDropdown.textContent == 'ALL') {
+        textDropdown.textContent = 'ВСЕ';
+    } else if (textDropdown.textContent == 'ВСЕ') {
+        textDropdown.textContent = 'ALL';
+    } else if (textDropdown.textContent == 'COMPLETE') {
+        textDropdown.textContent = 'ВЫПОЛНЕННЫЕ';
+    } else if (textDropdown.textContent == 'ВЫПОЛНЕННЫЕ') {
+        textDropdown.textContent = 'COMPLETE';
+    } else if (textDropdown.textContent == 'INCOMPLETE') {
+        textDropdown.textContent = 'НЕВЫПОЛНЕННЫЕ';
+    } else if (textDropdown.textContent == 'НЕВЫПОЛНЕННЫЕ') {
+        textDropdown.textContent = 'INCOMPLETE';
     }
 }
 
-// document.querySelector('.div-undo').addEventListener('click', () => {
-//     if (remote_note) {
-//         noteList.insertAdjacentHTML('beforeend', remote_note);
-//         temp_i++;
-//         document.querySelector('.loader').style.animation = 'none';
-//         document.querySelector('.div-undo').style.display = 'none';
-//     }
-// })
+function changeWidthMenu() {
+    if (textDropdown.textContent == 'ALL') {
+        btnDropdown.style.width = "94px";
+        searchInput.style.width = "595px";
+        document.querySelector('.menu').style.width = '90px';
+    } else if (textDropdown.textContent == 'ВСЕ') {
+        btnDropdown.style.width = "124px";
+        searchInput.style.width = "575px";
+        document.querySelector('.menu').style.width = '120px';
+    } else if (textDropdown.textContent == 'COMPLETE') {
+        btnDropdown.style.width = "120px";
+        searchInput.style.width = "569px";
+        document.querySelector('.menu').style.width = '116px';
+    } else if (textDropdown.textContent == 'ВЫПОЛНЕННЫЕ') {
+        btnDropdown.style.width = "170px";
+        searchInput.style.width = "519px";
+        document.querySelector('.menu').style.width = '166px';
+    } else if (textDropdown.textContent == 'INCOMPLETE') {
+        btnDropdown.style.width = "130px";
+        searchInput.style.width = "559px";
+        document.querySelector('.menu').style.width = '126px';
+    } else if (textDropdown.textContent == 'НЕВЫПОЛНЕННЫЕ') {
+        btnDropdown.style.width = "190px";
+        searchInput.style.width = "499px";
+        document.querySelector('.menu').style.width = '186px';
+    }
+}
